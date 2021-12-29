940e04fab0003503f0bffa3f1e536d531e55dae1
AdiEllis <adi.ellis3@gmail.com>
2021-12-29 13:26:21 +0200

    ll

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,31 +1,76 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Entities.BasicResponseModel;
+import com.example.demo.Models.File;
 import com.example.demo.Repositories.Repository;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
 
-    @Autowired public Repository repository;
-
+    @Autowired
+    public Repository repository;
+    EmailValidator validator = EmailValidator.getInstance();
 
     @CrossOrigin(origins = "http://localhost:3000")
-    @RequestMapping(value = "/demo/login", method = RequestMethod.POST)
-    public String login(@RequestParam String username, @RequestParam String password) {
-        String success = "failed";
-        if (username.equals(repository.loginUser.getUsername()) &&
-                password.equals(repository.loginUser.getPassword())) {
-            success = repository.loginUser.getToken();
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    public BasicResponseModel login(@RequestParam String email, @RequestParam String password) {
+        BasicResponseModel basicResponseModel = null;
+        if (!email.isEmpty() && !password.isEmpty()) {
+            if (validator.isValid(email)) {
+                if (email.equals(repository.loginUser.getEmail())) {
+                    if (password.length() > 7 &&
+                            password.matches("(.*[A-Z].*)") &&
+                            password.matches("(.*[a-z].*)") &&
+                            password.matches("(.*[0-9].*)") &&
+                            password.matches((".*[@,#,$,%].*$"))) {
+                        basicResponseModel = new BasicResponseModel("success!");
+                    } else {
+                        basicResponseModel = new BasicResponseModel(100, "Invalid password");
+                    }
+                }
+            }
         }
-        return success;
+        return basicResponseModel;
     }
 
+
     @CrossOrigin(origins = "http://localhost:3000")
-    @RequestMapping(value = "/demo/verifyLogin", method = RequestMethod.POST)
-    public String verifyLogin(@RequestParam String username, @RequestParam String token) {
+    @RequestMapping(value = "/api/getFiles", method = RequestMethod.GET)
+    public BasicResponseModel getFiles(@RequestParam String filter) {
+        BasicResponseModel basicResponseModel;
+        List<File> filterList;
+        filterList = repository.filesList.stream().filter((file ->
+                filter.equals(file.getFileName())
+                )).collect(Collectors.toList());
+        if (filterList.isEmpty()) {
+            basicResponseModel = new BasicResponseModel("File not found!");
+        } else {
+            basicResponseModel = new BasicResponseModel(filterList);
+        }
+        return basicResponseModel;
+    }
+
+
+//    @CrossOrigin(origins = "http://localhost:3000")
+//    @RequestMapping(value = "/api/getFiles", method = RequestMethod.GET)
+//    public BasicResponseModel getFiles(@RequestParam String sortBy, @RequestParam String sortType) {
+//        BasicResponseModel basicResponseModel;
+//
+//
+//        return basicResponseModel;
+//    }
+
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(value = "/api/verifyLogin", method = RequestMethod.POST)
+    public String verifyLogin(@RequestParam String email, @RequestParam String token) {
         String success = "failed";
-        if (username.equals(repository.loginUser.getUsername()) &&
+        if (email.equals(repository.loginUser.getEmail()) &&
                 token.equals(repository.loginUser.getToken())) {
             success = repository.loginUser.getToken();
         }
--- src/main/java/com/example/demo/Repositories/Repository.java
+++ src/main/java/com/example/demo/Repositories/Repository.java
@@ -1,6 +1,6 @@
 package com.example.demo.Repositories;
 
-import com.example.demo.Models.Product;
+import com.example.demo.Models.File;
 import com.example.demo.Models.User;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +9,9 @@ import java.util.List;
 
 @Component
 public class Repository {
-    public User loginUser = new User("209298454", "adi", "123", "masdam6masdkhadamasdnuas");
-    public List<Product> productList = new ArrayList<Product>(){{
-       add(new Product("Shampo", 120));
-       add(new Product("Kefli", 120));
-       add(new Product("Bamba", 120));
-       add(new Product("Chitos", 120));
+    public User loginUser = new User("adi.ellis3@gmail.com", "12345678Ae", "masdam6masdkhadamasdnuas");
+    public List<File> filesList = new ArrayList<File>(){{
+       add(new File(1, "MyFirstFile.txt", "7/12/2018"));
+       add(new File(2, "MyFirstFile2.txt", "7/13/2018"));
     }};
 }
